dd2ae495c7788bbafcf76ffcaef80bfa3684f836
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-07 17:14:24 -0500

    set up middleware and store in index.js and app.js

--- my-redux-app/src/index.js
+++ my-redux-app/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from "./reducers/reducer";
+
+const store = createStore(reducer, applyMiddleware(logger));
+console.log(store);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
