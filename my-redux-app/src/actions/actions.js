7bc999b174e2eec85b87fb077823026b22d30e3e
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-07 18:45:38 -0500

    added axios

--- my-redux-app/src/actions/actions.js
+++ my-redux-app/src/actions/actions.js
@@ -1,12 +1,25 @@
+import axios from "axios";
+
+const START_FETCHING = "START_FETCHING";
+const FETCH_SUCCESS = "FETCH_SUCCESS";
+const FETCH_FAILURE = "FETCH_FAILURE";
+
+
 const fetchFacts = () => {
-    return {}
-}
+    return (dispatch) => {
+        dispatch({ type: START_FETCHING })
+        // do the async action inside here and dispatch an error or success action
+        axios.get("http://cat-fact.herokuapp.com/facts")
+        .then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
+        .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.response }));
+    };
+};
 
 // redux thunk
-const thunk = next => action => store => {
-    if (typeof action === 'object') {
-        next(action); // forward action untouched
-    } else if (typeof action === "function") {
-        action() // stop the action and call the function
-    }
-}
+// const thunk = next => action => store => {
+//     if (typeof action === 'object') {
+//         next(action); // forward action untouched
+//     } else if (typeof action === "function") {
+//         action(store.dispatch) // stop the action and call the function
+//     }
+// }
