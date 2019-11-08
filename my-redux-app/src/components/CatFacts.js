16e13c04d18ee54b1c3daea493b3be4d99d3ce09
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-07 19:55:41 -0500

    everything set up, just need action creator

--- my-redux-app/src/components/CatFacts.js
+++ my-redux-app/src/components/CatFacts.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { connect } from "react-redux"; // bridge/connecting peice between react and redux
 
-const CatFacts = (props) => {
+import Fact from "./Facts"
+
+  {/* handle all possible state in JSX:
+            1) initial state
+            2) sucess state
+            3) loading state
+            4) error state */}
+
+const CatFacts = props => {
+    if (props.isFetching) {
+        return <h2>Loading Cat Facts...</h2>
+    } // handles loading state
     return (
         <div>
-
+            {props.state && <p>{props.error}</p>} {/* handles error state */}
+            {props.catFacts.map(fact => (
+                <Fact key={fact._id}/>
+            ))}
         </div>
     );
-}
+};
 
 const mapStateToProps = state => {
     return {
-        catFacts: state.catFacts
-    }
-}
+        catFacts: state.catFacts,
+        isFetching: state.isFetching,
+        error: state.error
+    };
+};
 
 export default connect(mapStateToProps, {})(CatFacts);
---
+++ my-redux-app/src/components/Facts.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Fact = props => {
+    return (
+        <p>{props.fact.text}</p>
+    )
+}
+
+export default Fact;
